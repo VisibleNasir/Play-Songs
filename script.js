f096f9786dd34f2d788ac8096fcea71a27556cf6
VisibleNasir <nasir7777786@gmail.com>
2024-12-22 17:33:01 +0530

    Added song playing functionalities we can play pause musics and songs are accessed from folder

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 console.log("Lets write js");
+let currentSong = new Audio();
 
+function secondsToMinutesSeconds(seconds){
+    if(isNaN(seconds) || seconds <0){
+        return "Invalid input";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remaininfSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2 , '0');
+    const formattedSeconds = String(remaininfSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
 
 async function getSongs() {
     let a = await fetch("http://127.0.0.1:5500/songs/");
@@ -20,16 +34,30 @@ async function getSongs() {
     return songs;
 }
 
-async function main(){
+const playMusic = (track, pause = false) =>{
+    // let audio = new Audio("/songs/" + track);
+    currentSong.src = "/songs/" + track;
+    if(!pause){
+        currentSong.play();
+        play.src = "img/pause.svg";
+    }
+    
+    document.querySelector(".songinfo").innerHTML = decodeURI(track)
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
+}
+
+async function main(){ 
+
     let songs = await getSongs();
-    console.log(songs);
+
+    playMusic(songs[0] , true)
 
     let songUl = document.querySelector(".songList").getElementsByTagName("ul")[0];
     for(const song of songs){
         songUl.innerHTML = songUl.innerHTML + `<li><img class="invert" src="img/music.svg" alt="" />
                 <div class="info">
                   <div>${song.replaceAll("%20" , " ")}</div>
-                  <div>Visible Nasir</div>
+                  <p>Visible Nasir</p>
                 </div>
                 <div class="playnow">
                   <span>Play Now</span>
@@ -37,12 +65,49 @@ async function main(){
                 </div>
                 </li>`;
     }
-    var audio = new Audio(songs[0]);
-    audio.play();  
+    // attach an event listener to each song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click" , element =>{
+            console.log(e.querySelector(".info").firstElementChild.innerHTML);
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
+        })
+        
+    }); 
 
-    audio.addEventListener("loadeddata" ,() =>{
-        let duration = audio.duration;
-        console.log(duration); 
+    // attach an event listener to play , next and previous
+    play.addEventListener("click" , ()=>{
+        if(currentSong.paused){
+            currentSong.play();
+            play.src = "img/pause.svg"
+        }else{
+            currentSong.pause();
+            play.src = "img/play.svg"
+        }
+    })
+    next.addEventListener("click",()=>{
+         
+    })
+    // Listen for time update event
+    currentSong.addEventListener("timeupdate" , ()=>{
+        console.log(currentSong.currentTime , currentSong.duration);
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`
+        document.querySelector(".circle").style.left = (currentSong.currentTime/currentSong.duration) * 100 + "%";
+    })
+
+    // Add an event to seekbar
+    document.querySelector(".seekbar").addEventListener("click" , e=>{
+        let percent = (e.offsetX/e.target.getBoundingClientRect().width)* 100
+        document.querySelector(".circle").style.left = percent + "%";
+        currentSong.currentTime = (currentSong.duration * percent)/100;
+    })
+
+    // add an EventListener for hamburger
+    document.querySelector(".hamburger").addEventListener("click" , ()=>{
+        document.querySelector(".left").style.left = "0";
+    })
+    // add reverse event listener on close 
+    document.querySelector(".close").addEventListener("click", ()=>{
+        document.querySelector(".left").style.left = "-100%";
     })
 } 
  
